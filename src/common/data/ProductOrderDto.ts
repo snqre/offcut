2d47de062ded3ffe7b01f9e99dacc5583cb75e2f
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2024-12-20 11:56:50 +0100

    fix product order dto

--- src/common/data/ProductOrderDto.ts
+++ src/common/data/ProductOrderDto.ts
@@ -9,13 +9,15 @@ export type ProductOrderDto = {
 };
 export function ProductOrderDto(_instance: ProductOrderDto):
     | Ok<ProductOrderDto>
-    | Err<"INVALID_AMOUNT">
+    | Err<"AMOUNT_BELOW_ZERO">
+    | Err<"AMOUNT_ABOVE_MAX_SAFE_INTEGER">
+    | Err<"AMOUNT_NOT_AN_INTEGER">
     | Err<"INVALID_INSTANCE"> {
     
     /** @constructor */ {
-        if (_instance.amount < 0) return Err("INVALID_AMOUNT");
-        if (_instance.amount > Number.MAX_SAFE_INTEGER) return Err("INVALID_AMOUNT");
-        if (!Number.isInteger(_instance.amount)) return Err("INVALID_AMOUNT");
+        if (_instance.amount < 0) return Err("AMOUNT_BELOW_ZERO");
+        if (_instance.amount > Number.MAX_SAFE_INTEGER) return Err("AMOUNT_ABOVE_MAX_SAFE_INTEGER");
+        if (!Number.isInteger(_instance.amount)) return Err("AMOUNT_NOT_AN_INTEGER");
         if (!ProductDtoSchema.safeParse(_instance).success) return Err("INVALID_INSTANCE");
         return Ok(_instance);
     }
